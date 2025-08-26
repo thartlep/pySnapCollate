4089a49bbf68096e500a3ff1cea109d95170c045
Thomas Hartlep <hartlep@baeri.org>
2025-08-25 18:29:12 -0700

    Added ability to update and remove daemon configurations

--- pySnapCollate/scripts/daemon.py
+++ pySnapCollate/scripts/daemon.py
@@ -14,6 +14,8 @@ import subprocess
 import json
 import re
 import glob
+import errno
+import shutil
 
 default_config_dir_name = "~/.pySnapCollate"
 default_queue = 'normal'
@@ -35,6 +37,11 @@ def setup_daemon(args):
     # Define the path for the configuration file
     config_path = os.path.join(daemon_dir, "config.json")
 
+    # Check if the configuration file exists
+    if os.path.exists(config_path) and not args.force:
+            print(f"Existing configuration file found for daemon '{args.name}'. Rerun with flag --force to override existing configuration.")
+            return
+        
     # Prepare the configuration data
     config_data = {
         "name": args.name,
@@ -166,7 +173,7 @@ def start_daemon(args):
         f"#PBS -r n\n",
         f"{environment}\n",
         f"cd {target}\n",
-        f"pySnapCollate -d {source} --varnames {varnames} --pvarnames {pvarnames} --verbose {verbose} --daemon_mode >> pySnapCollate.output \n"
+        f"pySnapCollate --directory {source} --varnames {varnames} --pvarnames {pvarnames} --verbose {verbose} --daemon_mode >> pySnapCollate.output \n"
     ]
 
     # Write run script to file
@@ -265,6 +272,53 @@ def list_daemons():
         print(f"An error occurred while listing daemons: {e}")
 
 ####################################################
+def remove_daemon(args):
+    config_dir = os.path.expanduser(default_config_dir_name) # Daemon configuration directory
+    daemon_dir = os.path.join(config_dir, args.name)  # Directory for the specific daemon
+    config_path = os.path.join(daemon_dir, "config.json") # Configuration file for the specific daemon
+    script_path = os.path.join(daemon_dir, "run.csh") # Run script for the specific daemon
+    active_pattern = os.path.join(daemon_dir, "active_job.*") # Acive daemo tag
+
+    # Check if the daemion directory
+    if not os.path.exists(daemon_dir):
+        print(f"No daemon named '{args.name}' found.")
+        return
+
+    # Check if daemon is active
+    already_in_queue = False
+    for active_file in glob.glob(active_pattern):
+        # Active daemon file found, let's check if daemon is actually queued or running
+        command = ["qstat", active_file.rsplit('.', 1)[-1]]
+        try:
+            result = subprocess.run(command, check=True, capture_output=True, text=True)
+            print(f"Daemon '{args.name}' is aready queued/running.")
+            already_in_queue = True
+        except subprocess.CalledProcessError as e:
+            # Old active daemon file exists but is no longeer queued or running, let's delete it
+            os.remove(active_file)
+
+    # Leave here if there is an active daemon queued/running
+    if already_in_queue:
+        print(f"Daemon is queue/running. Stop daemon first before removing.")
+        return
+    else: # otherwise delete directory for this daemon
+        for filepath in [config_path, script_path]:
+            if os.path.exists(filepath):
+                os.remove(filepath)
+        try:
+            os.rmdir(daemon_dir)
+        except OSError as e:
+            # Specific check for directory not being empty
+            if e.errno == errno.ENOTEMPTY:
+                if not args.force:
+                    print(f"Error: Cannot remove daemon directory because of unknown file(s). Use flag --force to delete anyways - {e}")
+                else:
+                    shutil.rmtree(daemon_dir)
+            else:
+                # Handle other potential OS-related errors
+                print(f"Unexpected OS error: {e}")
+
+####################################################
 def main():
     parser = argparse.ArgumentParser(description="Manage your pySnapCollate daemons.")
     subparsers = parser.add_subparsers(dest='command', required=True)
@@ -281,7 +335,8 @@ def main():
     setup_parser.add_argument('--queue', help='Name of scheduler queue (default: '+default_queue+')', default = default_queue)
     setup_parser.add_argument('--varnames', nargs = "*", help='Name(s) of variable(s) to export (default: '+' '.join(sorted(varname_list))+')', default = varname_list)
     setup_parser.add_argument('--pvarnames', nargs = "*", help='Name(s) of particle variable(s) name(s) to export (default: '+' '.join(sorted(pvarname_list))+')', default = pvarname_list)
-    setup_parser.add_argument('--verbose', action = 'store_true', help = 'verbose output (default: False)', default = False)
+    setup_parser.add_argument('--force', action = 'store_true', help = 'Forced override of existing daemon configuration (default: False)', default = False)
+    setup_parser.add_argument('--verbose', action = 'store_true', help = 'Verbose output (default: False)', default = False)
 
     # Start command
     start_parser = subparsers.add_parser('start', help='Start a daemon')
@@ -297,6 +352,11 @@ def main():
     inspect_parser = subparsers.add_parser('inspect', help='Inspect daemon configuration')
     inspect_parser.add_argument('--name', help='Name of the daemon', required=True)
 
+    # Remove command
+    remove_parser = subparsers.add_parser('remove', help='Remove a daemon, that is, delete the daemon configuration')
+    remove_parser.add_argument('--name', help='Name of the daemon', required=True)
+    remove_parser.add_argument('--force', action = 'store_true', help='Force deleting even with unknown files are in configuration directory', default = False)
+
     # List command
     list_parser = subparsers.add_parser('list', help='List all daemons')
 
@@ -310,6 +370,8 @@ def main():
         stop_daemon(args)
     elif args.command == 'inspect':
         inspect_daemon(args)
+    elif args.command == 'remove':
+        remove_daemon(args)
     elif args.command == 'list':
         list_daemons()  # Call without arguments
 
